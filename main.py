7b1b5ee72f2f7a64e46c5db26c530159b6c07391
Alex Cleveland <alexcleveland5@gmail.com>
2026-02-07 11:44:57 -0700

    Logic to grab a new spanish word on button click

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from tkinter import *
 import pandas as pd
+import random
+
 
 df = pd.read_csv("./data/spanish500.csv")
 data = df.to_dict(orient="records")
-print(data[0]["Spanish"])
+
 
 #Constants
 BACKGROUND_COLOR = "#B1DDC6"
 
+
 #Methods
 def new_word():
-    pass
+    global spanish_text
+    selection = random.choice(data)
+    spanish_word = selection["Spanish"]
+    canvas.itemconfig(spanish_text, text=spanish_word)
 
 #Window Setup
 window = Tk()
@@ -25,19 +31,18 @@ right_img = PhotoImage(file="./images/right.png")
 wrong_img = PhotoImage(file="./images/wrong.png")
 card_front_img = PhotoImage(file="./images/card_front.png")
 card_back_img = PhotoImage(file="./images/card_back.png")
-
-#Buttons
-right_button = Button(image=right_img, borderwidth=0, highlightthickness=0)
-wrong_button = Button(image=wrong_img, borderwidth=0, highlightthickness=0)
-right_button.grid(row=1, column=1)
-wrong_button.grid(row=1, column=0)
-
 #Canvas
 canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_img)
 canvas.create_text(400, 150, text="Spanish", font=("Ariel", 40, "italic"), fill="black")
-canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill="black")
 canvas.grid(row=0, column=0, columnspan=2)
+spanish_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"), fill="black")
+#Buttons
+right_button = Button(image=right_img, borderwidth=0, highlightthickness=0, command=new_word)
+wrong_button = Button(image=wrong_img, borderwidth=0, highlightthickness=0, command=new_word)
+right_button.grid(row=1, column=1)
+wrong_button.grid(row=1, column=0)
+
 
 
 
