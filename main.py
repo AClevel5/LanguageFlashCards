0e13cab6d319a4c4a318a5930a829bfa2309f749
Alex Cleveland <alexcleveland5@gmail.com>
2026-02-07 10:40:20 -0700

    Read CSV with Pandas

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from tkinter import *
+import pandas as pd
+
+df = pd.read_csv("./data/spanish500.csv")
+data = df.to_dict(orient="records")
+print(data[0]["Spanish"])
 
 #Constants
 BACKGROUND_COLOR = "#B1DDC6"
+
+#Methods
+def new_word():
+    pass
 
 #Window Setup
 window = Tk()
@@ -20,16 +29,16 @@ card_back_img = PhotoImage(file="./images/card_back.png")
 #Buttons
 right_button = Button(image=right_img, borderwidth=0, highlightthickness=0)
 wrong_button = Button(image=wrong_img, borderwidth=0, highlightthickness=0)
+right_button.grid(row=1, column=1)
+wrong_button.grid(row=1, column=0)
 
-
-
+#Canvas
 canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_img)
 canvas.create_text(400, 150, text="Spanish", font=("Ariel", 40, "italic"), fill="black")
 canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill="black")
 canvas.grid(row=0, column=0, columnspan=2)
-right_button.grid(row=1, column=1)
-wrong_button.grid(row=1, column=0)
+
 
 
 
