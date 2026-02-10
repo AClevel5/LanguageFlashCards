9f54e71bdbf65086182b8b531a5f78c41ac9d792
Alex Cleveland <alexcleveland5@gmail.com>
2026-02-09 21:05:35 -0700

    Flip card after 3 seconds

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 #Methods
 def new_word():
-    global spanish_text
+    global selection
     selection = random.choice(data)
     spanish_word = selection["Spanish"]
-    canvas.itemconfig(spanish_text, text=spanish_word)
+    canvas.itemconfig(word, text=spanish_word)
+    canvas.itemconfig(title, text="Spanish")
+
+def flip_card():
+    canvas.itemconfig(title, text="English")
+    canvas.itemconfig(word, text=selection["English"])
 
 #Window Setup
 window = Tk()
@@ -24,7 +29,7 @@ window.title("Learn Spanish")
 window.configure(background=BACKGROUND_COLOR)
 window.config(padx=50, pady=50)
 window.attributes("-topmost", True)
-
+window.after(3000, func=flip_card)
 
 #Import Images
 right_img = PhotoImage(file="./images/right.png")
@@ -34,16 +39,16 @@ card_back_img = PhotoImage(file="./images/card_back.png")
 #Canvas
 canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Spanish", font=("Ariel", 40, "italic"), fill="black")
+title = canvas.create_text(400, 150, text="Spanish", font=("Ariel", 40, "italic"), fill="black")
 canvas.grid(row=0, column=0, columnspan=2)
-spanish_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"), fill="black")
+word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"), fill="black")
 #Buttons
 right_button = Button(image=right_img, borderwidth=0, highlightthickness=0, command=new_word)
 wrong_button = Button(image=wrong_img, borderwidth=0, highlightthickness=0, command=new_word)
 right_button.grid(row=1, column=1)
 wrong_button.grid(row=1, column=0)
 
-
+new_word()
 
 
 
