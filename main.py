52294eda4a82db3f1f45218f114408b43f504c08
Alex Cleveland <alexcleveland5@gmail.com>
2026-02-09 21:13:10 -0700

    Change colors of card and text when flip, fix bug with timer to clear the first time if you click before 3 seconds.

--- main.py
+++ main.py
@@ -13,15 +13,19 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 #Methods
 def new_word():
-    global selection
+    global selection, flip_timer
+    window.after_cancel(flip_timer)
     selection = random.choice(data)
     spanish_word = selection["Spanish"]
-    canvas.itemconfig(word, text=spanish_word)
-    canvas.itemconfig(title, text="Spanish")
+    canvas.itemconfig(card_background, image=card_front_img)
+    canvas.itemconfig(word, text=spanish_word, fill="black")
+    canvas.itemconfig(title, text="Spanish", fill="black")
+    flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
-    canvas.itemconfig(title, text="English")
-    canvas.itemconfig(word, text=selection["English"])
+    canvas.itemconfig(title, text="English", fill="white")
+    canvas.itemconfig(word, text=selection["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 #Window Setup
 window = Tk()
@@ -29,7 +33,7 @@ window.title("Learn Spanish")
 window.configure(background=BACKGROUND_COLOR)
 window.config(padx=50, pady=50)
 window.attributes("-topmost", True)
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 #Import Images
 right_img = PhotoImage(file="./images/right.png")
@@ -38,7 +42,7 @@ card_front_img = PhotoImage(file="./images/card_front.png")
 card_back_img = PhotoImage(file="./images/card_back.png")
 #Canvas
 canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 title = canvas.create_text(400, 150, text="Spanish", font=("Ariel", 40, "italic"), fill="black")
 canvas.grid(row=0, column=0, columnspan=2)
 word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"), fill="black")
